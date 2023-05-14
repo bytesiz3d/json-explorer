50590900ed552f0dd128412894febf1ffa9175d0
bytesiz3d <abdelrahman.farid@gmail.com>
2023-05-14 12:28:59 +0300

    Test improvements

--- json-parser-tests/json-parser-tests.cpp
+++ json-parser-tests/json-parser-tests.cpp
@@ -33,7 +33,7 @@ namespace J_JSON_Tests
 
 				auto [json, err] = j_parse(file_content.c_str());
 				if (expect == EXPECT_PASS)
-					CHECK_MESSAGE(!err, std::string_view(err));
+					CHECK_MESSAGE(!err, err);
 				else if (expect == EXPECT_FAIL)
 				{
 					CAPTURE(file.string().c_str());
@@ -93,17 +93,142 @@ TEST_SUITE("Must Reject")
 	}
 }
 
-TEST_SUITE("Dump")
+TEST_SUITE("Manual")
 {
+	// REF: https://developer.spotify.com/documentation/web-api/reference/get-an-album
 	TEST_CASE("Dump")
 	{
-		J_JSON elements[]{
-			{.kind = J_JSON_NULL},
-			{.kind = J_JSON_BOOL, .as_bool = true},
-			{.kind = J_JSON_NUMBER, .as_number = 1.5}
-		};
-		J_JSON array{.kind = J_JSON_ARRAY, .as_array = {elements, 3}};
-		J_Pair pairs[]{{.key = "array", .value = array}};
-		MESSAGE(j_dump(J_JSON{.kind = J_JSON_OBJECT, .as_object = {.pairs = pairs, .count = 1}}));
+		auto [obj, err] = j_parse(R"(
+		{
+		  "album_type": "compilation",
+		  "total_tracks": 9,
+		  "available_markets": [
+			"CA",
+			"BR",
+			"IT"
+		  ],
+		  "external_urls": {
+			"spotify": "string"
+		  },
+		  "href": "string",
+		  "id": "2up3OPMp9Tb4dAKM2erWXQ",
+		  "images": [
+			{
+			  "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
+			  "height": 300,
+			  "width": 300
+			}
+		  ],
+		  "name": "string",
+		  "release_date": "1981-12",
+		  "release_date_precision": "year",
+		  "restrictions": {
+			"reason": "market"
+		  },
+		  "type": "album",
+		  "uri": "spotify:album:2up3OPMp9Tb4dAKM2erWXQ",
+		  "copyrights": [
+			{
+			  "text": "string",
+			  "type": "string"
+			}
+		  ],
+		  "external_ids": {
+			"isrc": "string",
+			"ean": "string",
+			"upc": "string"
+		  },
+		  "genres": [
+			"Egg punk",
+			"Noise rock"
+		  ],
+		  "label": "string",
+		  "popularity": 0,
+		  "artists": [
+			{
+			  "external_urls": {
+				"spotify": "string"
+			  },
+			  "followers": {
+				"href": "string",
+				"total": 0
+			  },
+			  "genres": [
+				"Prog rock",
+				"Grunge"
+			  ],
+			  "href": "string",
+			  "id": "string",
+			  "images": [
+				{
+				  "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
+				  "height": 300,
+				  "width": 300
+				}
+			  ],
+			  "name": "string",
+			  "popularity": 0,
+			  "type": "artist",
+			  "uri": "string"
+			}
+		  ],
+		  "tracks": {
+			"href": "https://api.spotify.com/v1/me/shows?offset=0&limit=20",
+			"limit": 20,
+			"next": "https://api.spotify.com/v1/me/shows?offset=1&limit=1",
+			"offset": 0,
+			"previous": "https://api.spotify.com/v1/me/shows?offset=1&limit=1",
+			"total": 4,
+			"items": [
+			  {
+				"artists": [
+				  {
+					"external_urls": {
+					  "spotify": "string"
+					},
+					"href": "string",
+					"id": "string",
+					"name": "string",
+					"type": "artist",
+					"uri": "string"
+				  }
+				],
+				"available_markets": [
+				  "string"
+				],
+				"disc_number": 0,
+				"duration_ms": 0,
+				"explicit": false,
+				"external_urls": {
+				  "spotify": "string"
+				},
+				"href": "string",
+				"id": "string",
+				"is_playable": false,
+				"linked_from": {
+				  "external_urls": {
+					"spotify": "string"
+				  },
+				  "href": "string",
+				  "id": "string",
+				  "type": "string",
+				  "uri": "string"
+				},
+				"restrictions": {
+				  "reason": "string"
+				},
+				"name": "string",
+				"preview_url": "string",
+				"track_number": 0,
+				"type": "string",
+				"uri": "string",
+				"is_local": false
+			  }
+			]
+		  }
+		})");
+
+		CHECK_MESSAGE(!err, err);
+		::puts(j_dump(obj));
 	}
 }
